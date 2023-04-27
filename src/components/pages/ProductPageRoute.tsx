6a7c12ba269b835cd2f05693775c4d6b17ab1013
unknown <gensukeSpp>
2023-04-27 12:09:30 +0900

    Thinking union or array.

--- src/components/pages/ProductPageRoute.tsx
+++ src/components/pages/ProductPageRoute.tsx
@@ -3,20 +3,28 @@ import { Rocon, Link } from 'rocon/react';
 import { productPageBuilder } from '../Routes';
 import { TopPage } from '../TopPage';
 
-const searchNum = window.location.search;
-
-let productPageArray: (string | null)[] = ["0", "1", "2"];
-productPageArray[Number(searchNum)] = null;
-
 const rootPath = Rocon.Path()
   .route("top", (route) => route.action(() => <TopPage />));
 
-const ProductPageMenu = () => {
+type ProductPageType = "0" | "1" | "2";
+// let productPageArray: (string | null)[] = ["0", "1", "2"];
+// productPageArray[Number(searchNum)] = null;
+
+let omitNum: string;
+type OmitPageIfSearch<T extends (string | null)> = T extends typeof omitNum ? null : T;
+type MenuItemArray = OmitPageIfSearch<ProductPageType>;
+
+const ProductPageMenu = (searchNumber: string, menuDisplay: MenuItemArray) => {
+	// const searchNum = window.location.search;
+
 	return (
 		<ul>
 			<li>
 				<Link route={rootPath._.top} className={menuItem.anchor}>戻る</Link>
 			</li>
+			{menuDisplay.flatMap(() => {
+				
+			})}
 			<li>
 				<Link route={productPageBuilder.route} match={{"page": "1"}} className={menuItem.anchor}>作品２</Link>
 			</li>
--- src/components/pages/RetrieveCapture.tsx
+++ src/components/pages/RetrieveCapture.tsx
@@ -2,18 +2,17 @@ import capture1 from '../../image/sunkit_cap1.png';
 import capture3 from '../../image/portfolio_cap3.png';
 
 const captures = [capture1, capture3];
-type valueOf<T> = T[keyof T];
+// type valueOf<T> = T[keyof T];
 
-const property_type: {[key: number]: string} = {
-	0: capture1,
-	1: capture3
-} as const;
+// const property_type: {[key: number]: string} = {
+// 	0: capture1,
+// 	1: capture3
+// } as const;
 
-// type ImagePath = valueOf<typeof property_type>;
-type ImagePath = typeof property_type[keyof typeof property_type];
+// // type ImagePath = valueOf<typeof property_type>;
+// type ImagePath = typeof property_type[keyof typeof property_type];
 export type NewKey = 0 | 1;
 
-// type U<T> = T extends number ? T : number;
 type Prop = {
 	keyNumber: NewKey,
 	title: string
@@ -26,5 +25,4 @@ export const TopCaptureComponent = (prop: Prop) => {
 			<img src={captures[prop.keyNumber]} alt={prop.title + "のスクリーンショット"} />
 		</div>
 	);
-	
 }
