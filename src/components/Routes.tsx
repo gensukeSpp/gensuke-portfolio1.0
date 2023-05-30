83059415e8cd55fd1d43872167d8e236757a3249
unknown <gensukeSpp>
2023-05-30 12:04:36 +0900

    Commit 5

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -3,13 +3,13 @@ import { TopPage } from '~/components/TopPage';
 import { ProductComponent } from './pages/ProductPage';
 
 const topLevelRoutes = Rocon.Path()
-  .exact({ action: () => <p>I am root</p> })
+  .exact({ action: () => <TopPage /> })
   // .route("sunkit", (route) => route.action(() => <ProductPage page={page} />)
-  // .route("products");
+  .route("products");
 
-// export const productPageBuilder = topLevelRoutes._.products
-//   .attach(Rocon.Search("page"))
-//   .action(({page}) => <ProductComponent pageNum={Number(page)} />);
+export const productPageBuilder = topLevelRoutes._.products
+  .attach(Rocon.Search("page"))
+  .action(({page}) => <ProductComponent pageNum={Number(page)} />);
 
 export const Routes = () => {
   return useRoutes(topLevelRoutes);
