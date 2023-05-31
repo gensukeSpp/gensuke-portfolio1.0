caede36f595c7e3bf495eeaab80237af0e03e2d3
unknown <gensukeSpp>
2023-05-31 12:04:15 +0900

    commit 0531 4

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -2,18 +2,23 @@ import { Rocon, useRoutes } from 'rocon/react';
 import { TopPage } from '~/components/TopPage';
 import { ProductComponent } from './pages/ProductPage';
 
-const topLevelRoutes = Rocon.Path()
-  .routes({
-    foo: {
-      action: () => <TopPage />
-    },
-    products: {}
-  });
+const docsRoutes = Rocon.SingleHash("hash", { optional: true })
+  .attach(Rocon.Path())
+  .exact({
+    action: () => <p>It's root page.</p>,
+  })
+  .route("products");
 
-export const productPageBuilder = topLevelRoutes._.products
+export const productPageBuilder = docsRoutes._.products
   .attach(Rocon.Search("page"))
   .action(({page}) => <ProductComponent pageNum={Number(page)} />);
 
+const topLevelRoutes = Rocon.Path()
+  .exact({
+    action: () => <TopPage />,
+  })
+  .route("docs", (p) => p.attach(docsRoutes));
+
 export const Routes = () => {
-  return useRoutes(productPageBuilder);
+  return useRoutes(topLevelRoutes);
 };
