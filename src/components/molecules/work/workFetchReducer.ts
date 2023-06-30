c3d5e2ff44a8fa0479ed38f3991336181ba3da11
unknown <gensukeSpp>
2023-06-30 14:49:24 +0900

    chore #230630: reduce file?

--- src/components/molecules/work/workFetchReducer.ts
+++ src/components/molecules/work/workFetchReducer.ts
@@ -1,13 +1,19 @@
+import { Dispatch, createContext, useReducer } from 'react';
 import { Work } from '../../pages/portfolio_type';
+
+// type WorksState = Work[];
+const WorksStateContext = createContext<Work[] | undefined>(undefined);
 
 type Action = {
   type: 'output';
-  state: Work;
+  search: number;
 };
-const initialWork: Work = {
-  title: '',
-  term: '',
-  presentation: '',
-  language: [],
-  link: '',
+type WorksDispatch = Dispatch<Action>;
+const WorksDispatchContext = createContext<WorksDispatch | undefined>(undefined);
+
+const worksReducer = (state: Work[], action: Action): Work[] => {
+  switch (action.type) {
+    case 'output':
+      return state.concat(state[action.search]);
+  }
 };
